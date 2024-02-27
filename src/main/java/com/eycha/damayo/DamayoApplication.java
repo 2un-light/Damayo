253e214961b1e80c72b4c32442164ab3b42526fa
차은영 <sts07129@naver.com>
2024-02-27 16:13:09 +0900

    setting : 개발환경 설정 (#2)

---
+++ src/main/java/com/eycha/damayo/DamayoApplication.java
@@ -0,0 +1,13 @@
+package com.eycha.damayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamayoApplication.class, args);
+	}
+
+}
